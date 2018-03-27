705805560484f6c965857c61995cbf78a8da122a
indyc-immalle <31651150+indyc-immalle@users.noreply.github.com>
2018-03-27 10:40:19 +0200

    Add gewicht soort

---
+++ DierenInDeZoo/Dier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DierenInDeZoo
+{
+    abstract class Dier
+    {
+        public double Gewicht { get; set; }
+        public string GewichtBeschrijving
+        {
+            get
+            {
+                if (Gewicht > 100)
+                {
+                    return " wat een zwaargewicht";
+                }
+                else
+                {
+                    return " wat een lichtgewicht";
+                }
+            }
+           
+        }
+        public void PrintBeschrijving()
+        {
+            if (Gewicht > 100)
+            {
+                Console.WriteLine("Dat is een zwaargewicht");
+            }
+            else
+            {
+                Console.WriteLine("Dat is een lichtgewicht");
+            }
+        }
+        public virtual void MaakGeluid()
+        {
+            Console.WriteLine("<stilte>");
+           
+        }
+
+        public virtual void NaamDier()
+        {
+            Console.WriteLine("das een dier");
+        }
+    }
+
+
+}
--- DierenInDeZoo/Program.cs
+++ DierenInDeZoo/Program.cs
@@ -5,10 +5,82 @@ using System.Text;
 
 namespace DierenInDeZoo
 {
+
+    class Paard : Dier
+    {
+        public override void MaakGeluid()
+        {
+            Console.WriteLine("hinnik hinnik");
+        }
+
+        public override void NaamDier()
+        {
+            Console.WriteLine("Mijn Naam is Skiaaa ");
+        }
+    }
+    
+    class Varken : Dier
+    {
+        public override void MaakGeluid()
+        {
+            Console.WriteLine("knor knor");
+        }
+
+        public override void NaamDier()
+        {
+            Console.WriteLine("Mijn Naam is Alfred het varken ");
+        }
+    }
+
+    class Koe : Dier
+    {
+        public override void MaakGeluid()
+        {
+            Console.WriteLine("moeeee");
+        }
+        public override void NaamDier()
+        {
+            Console.WriteLine("Mijn Naam is Skroe de koe");
+        }
+    }
+
+    class Mol : Dier
+    {
+        public override void MaakGeluid()
+        {
+            Console.WriteLine("skrkrksrkrkrskrks");
+        }
+
+        public override void NaamDier()
+        {
+            Console.WriteLine("Mijn Naam is Pol de mol");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Dier p = new Paard();
+            Dier v = new Varken();
+            Dier k = new Koe();
+            Dier m = new Mol();
+
+            p.Gewicht = 102.2;
+            p.MaakGeluid();
+            p.NaamDier();
+            Console.WriteLine("Ik weeg {0}", p.Gewicht);
+            Console.WriteLine(k.GewichtBeschrijving);
+            p.PrintBeschrijving();
+
+
+            v.Gewicht = 67.3;
+            v.MaakGeluid();
+            v.NaamDier();
+            Console.WriteLine("Ik weeg {0}", v.Gewicht);
+
+            k.NaamDier();
+
+            m.NaamDier();
         }
     }
 }
